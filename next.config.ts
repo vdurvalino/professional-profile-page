23ba4f2decb3fb3ac620b28e6b0875dc3d2144dd
Vinícius D. de Souza <64796161+vdurvalino@users.noreply.github.com>
2025-09-03 21:35:07 -0300

    feat: add social media redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/vinicius-d-de-souza-b745a41bb/',
+                permanent: false,
+            },
+            {
+                source: '/github',
+                destination: 'https://github.com/vdurvalino/',
+                permanent: false,
+            },
+            {
+                source: '/discord',
+                destination: 'https://discordapp.com/users/710987761661902990',
+                permanent: false,
+            },
+        ]
+    }
 };
 
 export default nextConfig;
