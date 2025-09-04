0ede9898c8dd23141523884ac56a5f72552c7cb7
Vinícius Durvalino de Souza <viniciusd.souza@hotmail.com>
2025-09-04 19:03:20 -0300

    refactor: migrate i18n to Contentful

--- src/stores/app-store.ts
+++ src/stores/app-store.ts
@@ -1,51 +1,15 @@
 import {create} from 'zustand';
 
-import {pt} from '@/locales/pt';
-import {en} from '@/locales/en'; // Import en as well for language switching
-
-export type LocaleSchema = {
-    greeting: string;
-    status: string;
-    intro: string;
-    building: string;
-    features: string[];
-    workingOn: string;
-    tech: string[];
-    contact: string;
-    email: string;
-    subscribe: string;
-    emailPlaceholder: string;
-    notify: string;
-    footer: string;
-    launched: string;
-    emailRequired: string;
-    emailInvalid: string;
-    emailSentSuccess: string;
-    emailSentErrorGeneric: string;
-    emailSentErrorConnection: string;
-    sending: string;
-    blog: string
-    seeAll: string
-    latestArticles: string
-};
-
-const translations = {pt, en};
-
 interface AppState {
     isDarkMode: boolean;
-    language: "en" | "pt";
-    t: LocaleSchema;
 
     toggleDarkMode: () => void;
-    setLanguage: ( lang: "en" | "pt" ) => void;
     initializeTheme: () => void;
 }
 
 // Criação do store usando zustand
 export const useAppStore = create<AppState>(( set ) => ({
     isDarkMode: false,
-    language: 'pt',
-    t: pt,
 
     toggleDarkMode: () => {
         set(( state ) => {
@@ -58,13 +22,6 @@ export const useAppStore = create<AppState>(( set ) => ({
                 document.documentElement.classList.remove('dark');
             }
             return {isDarkMode: newDarkMode};
-        });
-    },
-
-    setLanguage: ( lang ) => {
-        set({
-            language: lang,
-            t: translations[lang] as LocaleSchema // Ensure type correctness
         });
     },
 
