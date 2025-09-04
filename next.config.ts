beec3a904de9244be65d8919c54ab4c83409fcfa
Vinícius Durvalino de Souza <viniciusd.souza@hotmail.com>
2025-09-04 19:06:53 -0300

    refactor: use social media constants for redirects

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,23 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import {DISCORD, GITHUB, LINKEDIN} from "@/constants/social";
 
 const nextConfig: NextConfig = {
     async redirects() {
         return [
             {
                 source: '/linkedin',
-                destination: 'https://www.linkedin.com/in/vinicius-d-de-souza-b745a41bb/',
+                destination: LINKEDIN,
                 permanent: false,
             },
             {
                 source: '/github',
-                destination: 'https://github.com/vdurvalino/',
+                destination: GITHUB,
                 permanent: false,
             },
             {
                 source: '/discord',
-                destination: 'https://discordapp.com/users/710987761661902990',
+                destination: DISCORD,
                 permanent: false,
             },
         ]
