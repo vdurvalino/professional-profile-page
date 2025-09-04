fb276f2fa4987311cc710e6b1f113e451865183f
Vinícius Durvalino de Souza <viniciusd.souza@hotmail.com>
2025-09-04 18:55:44 -0300

    feat: add social media redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/vinicius-d-de-souza-b745a41bb/',
+                permanent: false,
+            },
+            {
+                source: '/github',
+                destination: 'https://github.com/vdurvalino/',
+                permanent: false,
+            },
+            {
+                source: '/discord',
+                destination: 'https://discordapp.com/users/710987761661902990',
+                permanent: false,
+            },
+        ]
+    }
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
